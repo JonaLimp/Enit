4d7b09b01d2980b2be213224448aa884f3ae4b06
Jonas Limpert <33454605+JonaLimp@users.noreply.github.com>
2024-11-26 12:07:54 +0100

    Add debug panel

--- enit/settings.py
+++ enit/settings.py
@@ -149,3 +149,9 @@ CELERY_TIMEZONE = "UTC"
 # settings.py
 CELERY_BROKER_URL = "redis://localhost:6379/0"
 CELERY_RESULT_BACKEND = "redis://localhost:6379/0"
+
+# Add debug toolbar
+INSTALLED_APPS += ["debug_toolbar"]
+MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
+
+INTERNAL_IPS = ["127.0.0.1"]
