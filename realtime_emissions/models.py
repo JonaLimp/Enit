3fe8553c7bbca6a15641be4ae6e2869b501cc6de
Jonas Limpert <33454605+JonaLimp@users.noreply.github.com>
2024-11-27 09:37:18 +0100

    Bundle core functionalities in core app

--- realtime_emissions/models.py
+++ realtime_emissions/models.py
@@ -1,12 +1,5 @@
 from django.db import models
-
-
-class Region(models.Model):
-    name = models.CharField(max_length=100, unique=True)
-    code = models.CharField(max_length=10, unique=True)
-
-    def __str__(self) -> str:
-        return self.name
+from ..enit.models import Region
 
 
 class RealtimeEmissionRecord(models.Model):
